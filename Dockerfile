d5a7aef4f33d233b62a5a120482d84bf8e174a62
Shivam Bansal <shivambansal2050@gmail.com>
2024-04-15 10:53:42 +0530

    second_commit

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
+
 FROM node:14
-
-WORKDIR /node_learning/checking
-
+WORKDIR /app
 COPY package*.json ./
-
 RUN npm install
-
 COPY . .
-
 EXPOSE 3000
-
 CMD ["node", "check.js"]
+
+# docker ps
+# docker build -t shivambansal1000/nodeapp
+# docker run -d -p 3000:3000 shivambansal1000/nodeapp
+
+
+# docker push shivambansal1000/nodeapp:latest
--- check.js
+++ check.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-app.use = (express.json());
+const port = 3000;
 
-app.get("/", (req, res) => {
-    console.log("hello world");
+app.get('/', (req, res) => {
+  res.send('Hello, this is your Node.js application running on port 3000!');
 });
-
-app.listen(3000);
+app.get('/check',(req, res) => {
+  res.send("inside check"); 
+})
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
